93a980cf3a455dfef227587ef891849230579707
Tiarnan McGrath <mcgratti@tcd.ie>
2016-04-01 14:07:34 +0100

    Login view and graph view

--- website/app/graphView/graphView.js
+++ website/app/graphView/graphView.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.graphView', ['ngRoute'])
+angular.module('myApp.graphView', ['ngRoute','chart.js'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/graph', {
@@ -9,6 +9,14 @@ angular.module('myApp.graphView', ['ngRoute'])
   });
 }])
 
-.controller('GraphViewController', [function() {
-
+.controller('GraphViewController', ['$scope',function($scope) {
+	$scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+	$scope.series = ['Series A', 'Series B'];
+	$scope.data = [
+		[65, 59, 80, 81, 56, 55, 40],
+		[28, 48, 40, 19, 86, 27, 90]
+	];
+	$scope.onClick = function (points, evt) {
+		console.log(points, evt);
+	};
 }]);
